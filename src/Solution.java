e41cbcc4b8b052bb417fa044e14aae08f18245fb
Crow <oneso123456789@gmail.com>
2023-01-11 16:27:57 +0900

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+class Solution {
+    public static int solution(int storey) {
+        int answer = 0;
+        int[] aa = convertIntToArray(storey);
+        for (int i = aa.length - 1; i >= 0; i--) {
+            System.out.println("배열" + Arrays.toString(aa));
+            System.out.println("현재 a값:" + aa[i]);
+            int cal = Math.abs((10 - aa[i]));
+            System.out.println("cal:" + cal);
+            if (cal < 6 && i != 0) {
+                answer += cal;
+                System.out.println("if 에서 answer" + answer);
+                aa[i - 1] += 1;
+            } else if (cal < 6) {
+                answer += cal + 1;
+                System.out.println("else if 에서 도는 answer" + answer);
+            } else {
+                answer += aa[i];
+                System.out.println("else 에서 도는 answer" + answer);
+            }
+        }
+        return answer;
+    }
+
+    public static int[] convertIntToArray(int number) {
+
+        return Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(95));
+    }
+}
---
+++ src/beat.java
@@ -0,0 +1,35 @@
+public class beat {
+    public static int solution(int n, long l, long r) {
+        return (convert(r) - convert(l - 1));
+    }
+
+    public static int convert(long n) {
+        if (n <= 5) {
+            return (int) "11011".substring(0, (int) n).replaceAll("0", "").length();
+        }
+
+        long base = 1;
+        while ((long) Math.pow(5, (base + 1)) < n) {
+            base += 1;
+        }
+
+        long section = (long) (n / (Math.pow(5, base)));
+        long remainder = (long) (n % (Math.pow(5, base)));
+        long answer = (long) (section * (Math.pow(4, base)));
+
+        if (section >= 3) {
+            answer -= (Math.pow(4, base));
+        }
+
+        if (section == 2) {
+            return (int) answer;
+        } else {
+            return (int) (answer + convert(remainder));
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(1, 1, 5));
+
+    }
+}

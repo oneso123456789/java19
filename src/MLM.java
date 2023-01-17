806367db1d33ae32d35c58cd61eef9706c496707
Crow <oneso123456789@gmail.com>
2023-01-17 19:58:18 +0900

    0.1원 단위는 배분 안하게 하면 완료

--- src/MLM.java
+++ src/MLM.java
@@ -1,9 +1,17 @@
 import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.Objects;
 
 public class MLM {
 
-    public class Profit {
+    private class Profit {
+        private String referral;
+        private int profit;
+
+        public Profit() {
+
+        }
+
         public Profit(String referral, int profit) {
             this.referral = referral;
             this.profit = profit;
@@ -25,32 +33,53 @@ public class MLM {
             this.profit = profit;
         }
 
-        private String referral;
-        private int profit;
-
-
+        public void addProfit(int add) {
+            this.profit = profit + add;
+        }
     }
 
-    public int[] solution(String[] enroll, String[] referral,
-                          String[] seller, int[] amount) {
-        LinkedHashMap<String, Profit> hashMap = new LinkedHashMap<String, Profit>();
-        System.out.println(Arrays.toString(enroll));
-        for (int i = 0; i < enroll.length - 1; i++) {
-            hashMap.put(enroll[i], new Profit(referral[i], 0));
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        System.out.println(Arrays.toString(seller));
+        LinkedHashMap<String, Profit> member = makeMemberMap(enroll, referral);
+        Profit currentProfit = new Profit();
+        Profit referralProfit = new Profit();
+
+        for (int i = 0; i < seller.length; i++) {
+            currentProfit = member.get(seller[i]);
+            int salesAmount = (amount[i] * 100);
+            currentProfit.addProfit(salesAmount);
+            member.replace(seller[i], currentProfit);
         }
 
-        hashMap.forEach((s, profit) -> System.out.println(s + " : " + profit.referral + "&" + profit.profit));
+        for (int i = enroll.length - 1; i > -1; i--) {
+            currentProfit = member.get(enroll[i]);
+            if (!Objects.equals(currentProfit.getReferral(), "-")){
+                referralProfit = member.get(currentProfit.referral);
+                referralProfit.addProfit((int) (currentProfit.profit * 0.1));
+            }
+            currentProfit.setProfit((int) (currentProfit.getProfit() * 0.9));
+            System.out.println("얼마를 가졌나? + " + currentProfit.profit);
 
+        }
+        member.forEach((s, profit) -> System.out.println(s + ":" + profit.profit));
         int[] answer = {};
         return answer;
     }
 
+    private LinkedHashMap<String, Profit> makeMemberMap(String[] enroll, String[] referral) {
+        LinkedHashMap<String, Profit> member = new LinkedHashMap<>();
+        int cnt = 0;
+        for (int i = 0; i < enroll.length; i++) {
+            cnt++;
+            member.put(enroll[i], new Profit(referral[i], 0));
+        }
+        System.out.println(cnt);
+        return member;
+    }
+
     public static void main(String[] args) {
         MLM mlm = new MLM();
-        System.out.println(Arrays.toString(mlm.solution(new String[]{"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"}
-                , new String[]{"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"},
-                new String[]{"young", "john", "tod", "emily", "mary"},
-                new int[]{12, 4, 2, 5, 10})));
+        System.out.println(Arrays.toString(mlm.solution(new String[]{"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"}, new String[]{"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"}, new String[]{"young", "john", "tod", "emily", "mary"}, new int[]{12, 4, 2, 5, 10})));
     }
 
 
